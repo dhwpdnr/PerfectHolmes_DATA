44202297e9e73172915aca45ca89bae21fb5ba27
dhwpdnr <ink1237@gmail.com>
2023-12-04 11:26:03 +0900

    [feat] set data command

--- utils.py
+++ utils.py
@@ -7,17 +7,27 @@ def get_location(address):
     load_dotenv(verbose=True)
     KAKAO_API_KEY = os.getenv('KAKAO_API_KEY')
     headers = {"Authorization": f"KakaoAK {KAKAO_API_KEY}"}
-    test_data = pd.read_csv('data/test_data.csv', encoding='utf-8')
+    test_data = pd.read_csv('data/school_data.csv', encoding='utf-8')
+    response_data = []
     for item in test_data.itertuples():
-        url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + item[1]
-        # for i in range(0, 5070, 1):
-        #     split = []
-        #     if test_data['addr'][i].endswith(']'):
-        #         split = test_data['addr'][i].split('[')  # split은 리스트 반환
-        #         test_data['addr'][i] = split[0]
+        url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + item[4]
         api_json = json.loads(str(requests.get(url, headers=headers).text))
-        print(api_json['documents'][0]['address']['x'])
-        print(api_json['documents'][0]['address']['y'])
-        # print(f"x : {api_json['documents']['address']['x']}, y : {api_json['documents']['address']['y']}")
+        school_data = {}
+        if api_json['documents'] == []:
+            continue
+        if "유치원" in str(item[1]):
+            school_data["type"] = "유치원"
+        elif "초등학교" in str(item[1]):
+            school_data["type"] = "초등학교"
+        elif "중학교" in str(item[1]):
+            school_data["type"] = "중학교"
+        elif "고등학교" in str(item[1]):
+            school_data["type"] = "고등학교"
 
-    return api_json
+        school_data["name"] = item[1]
+        school_data["address"] = item[4]
+        school_data["lat"] = api_json['documents'][0]['address']['x']
+        school_data["lng"] = api_json['documents'][0]['address']['y']
+        response_data.append(school_data)
+
+    return response_data
